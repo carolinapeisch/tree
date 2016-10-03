353b001cc07935b1e51494ffa552fe2cb49e31aa
Carolina Peisch <carolinapeisch@gmail.com>
2016-10-03 11:58:44 +0000

    still

--- pytree.py
+++ pytree.py
@@ -12,12 +12,10 @@ fifth = "`--"
 file_count = 0
 dirs_count = 0
 second_count = 0
-# another_count = 0
-# third_count = 0
 last_count = 0
 
 
-def find_files(path):
+def find_last(path):
     for root, dirs, files in os.walk(path):
         levels = root.split('/')
         rlevels = len(levels) - 1
@@ -45,15 +43,14 @@ def find_tree(path):
         global file_count
         global dirs_count
         global second_count
-        # global another_count
-        # global third_count
         for f in files:
             file_count += 1
             if file_count == 1:
                 if rlevels == 1:
                     print(first, f)
-            for d in dirs:
-                dirs_count += 1
+        for d in dirs:
+            dirs_count += 1
+            # if dirs_count == 2:
             if rlevels == 1:
                 print(first, d)
             elif rlevels == 2:
